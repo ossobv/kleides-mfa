c74510b12cf0b3ebfc2c01202025968b0b3f5c3e
Harm Geerts <hgeerts@osso.nl>
2019-11-06 14:02:35 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     keywords='kleides_mfa',
     name='kleides_mfa',
     packages=find_packages(include=['kleides_mfa', 'kleides_mfa.*']),
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
