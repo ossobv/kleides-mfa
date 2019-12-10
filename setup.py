fe5e3bfa185d1fde026f1d3d64df88c3b0fe2940
Harm Geerts <hgeerts@osso.nl>
2019-12-10 15:42:37 +0100

    Remove sphinx class from readme for PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    readme = readme_file.read().replace(':class:`~', '`')
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
