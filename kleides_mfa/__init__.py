2e220b70bf4244bebb7a3a1f298b5944d926c1bb
Harm Geerts <hgeerts@osso.nl>
2024-02-19 10:49:22 +0100

    Remove <Django-3.2 app config conditional

--- kleides_mfa/__init__.py
+++ kleides_mfa/__init__.py
@@ -5,8 +5,3 @@
 __author__ = """Harm Geerts"""
 __email__ = 'hgeerts@osso.nl'
 __version__ = '0.1.14'
-
-import django
-
-if django.VERSION < (3, 2):
-    default_app_config = 'kleides_mfa.apps.KleidesMfaConfig'
